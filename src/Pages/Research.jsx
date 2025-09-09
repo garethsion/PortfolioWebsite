3cac1f0cd054fe5b413b141755e2835d05e0e925
Gareth Jones <garetrhsion@googlemail.com>
2025-09-09 20:18:54 +0100

    Updating Research

--- src/Pages/Research.jsx
+++ src/Pages/Research.jsx
@@ -6,120 +6,49 @@ import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
-import { NavLink, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import NV_EnergyLevels from './Research/figs/NV_EnergyLevels.png';
 import quantum_sensing from './Research/figs/quantum_sensing.png';
-import sc_devices from './Research/figs/supercond_devices.png'
+import sc_devices from './Research/figs/supercond_devices.png';
+import Grid from '@mui/material/Grid';
 
 export default function Research() {
-
   const navigate = useNavigate();
 
+  const cards = [
+    { title: 'Solid State Defects in Diamond', img: NV_EnergyLevels, link: '/diamonddefects' },
+    { title: 'Quantum Enhanced Metrology', img: quantum_sensing, link: '/quantumsensing' },
+    { title: 'Superconducting Devices', img: sc_devices, link: '/superconddevices' },
+    { title: 'Cavity Quantum Electrodynamics', img: sc_devices, link: '/superconddevices' },
+    { title: 'Electro-Optic Thin-Films', img: sc_devices, link: '/superconddevices' }
+  ];
+
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ marginRight: '20px' }}>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            src={NV_EnergyLevels}
-            // image='https://www.anl.gov/sites/www/files/styles/article_teaser_16x9/public/2019-09/Argonne_MagnonScienceHighlight_Draft02_1600x900.png?h=8abcec71&itok=yw298ns8)'
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Solid State Defects in Diamond
-            </Typography>
-            {/* <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              A review of analytic methods for designing superconducting coplanar waveguide resonators, with supporting python code.
-            </Typography> */}
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary" onClick={()=> navigate('/diamonddefects')}>Go</Button>
-        </CardActions>
-      </Card>
-      </div>
-
-      <div style={{ marginRight: '20px' }}>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            src={quantum_sensing}
-            // image='https://www.anl.gov/sites/www/files/styles/article_teaser_16x9/public/2019-09/Argonne_MagnonScienceHighlight_Draft02_1600x900.png?h=8abcec71&itok=yw298ns8)'
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Quantum Enhanced Metrology
-            </Typography>
-            {/* <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              A review of analytic methods for designing superconducting coplanar waveguide resonators, with supporting python code.
-            </Typography> */}
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary" onClick={()=> navigate('/quantumsensing')}>Go</Button>
-        </CardActions>
-      </Card>
-      </div>
-      
-      <div style={{ marginRight: '20px' }}>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            src={sc_devices}
-            // image='https://www.anl.gov/sites/www/files/styles/article_teaser_16x9/public/2019-09/Argonne_MagnonScienceHighlight_Draft02_1600x900.png?h=8abcec71&itok=yw298ns8)'
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Superconducting Devices
-            </Typography>
-            {/* <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              A review of analytic methods for designing superconducting coplanar waveguide resonators, with supporting python code.
-            </Typography> */}
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary" onClick={()=> navigate('/superconddevices')}>Go</Button>
-        </CardActions>
-      </Card>
-      </div>
-
-    </div>
-
-    
+    <Grid container mt={50} rowSpacing={4} sm={8}>
+      {cards.map((card, index) => (
+        <Grid item xs={12} sm={6} key={index}>
+          <Card sx={{ maxWidth: 345, margin: 'auto' }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                src={card.img}
+                alt={card.title}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {card.title}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary" onClick={() => navigate(card.link)}>
+                Go
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   );
 }
-
-
-
-// import React from 'react'
-// import { Grid, Box } from '@mui/material';
-
-// const Research = () => {
-//   return (
-//     <Grid className="tutorial-resonators" container mt={10} mr={20} ml={20}>
-//       <Grid item xs={12}>
-//         <Box>
-
-//         <p>Quantum Networks</p>
-//         <p>Solid State Defects in Diamond </p>
-//         <p>Electro-Optic Materials</p>
-//         <p>Quantum Sensing</p>
-//         <p>Superconducting Devices</p>
-//         </Box>
-//       </Grid>
-//     </Grid>
-//   )
-// }
-
-
-// export default Research
